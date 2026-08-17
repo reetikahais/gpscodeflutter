77f207ba3b224bbd3ab5bcc8743658aa73bdba1e
LAPTOP-02Reetika <reetika@highaltsolutions.in>
2026-08-17 17:34:34 +0530

    fix(flutter): migrate signal_info plugin off deprecated kotlinOptions
    kotlinOptions{} is error-level deprecated on the AGP/Kotlin version
    codemagic's flutter-stable now resolves, failing the codemagic build.
    Use the compilerOptions DSL instead.

--- packages/signal_info/android/build.gradle.kts
+++ packages/signal_info/android/build.gradle.kts
@@ -15,8 +15,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+}
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
     }
 }
